b206ef55236d16780ace323e51a431b7289e0764
josh <josh.zollweg@gmail.com>
2015-07-24 02:26:13 +0000

    Broke Messager class into more specific Sms and Email classes for easier handling in SecurityCamera class

---
+++ messaging/Email.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import smtplib
+
+"""Email.py
+
+Class that sends emails from a gmail account.
+
+"""
+
+class Email(object):
+    """Main class for sending email messages. 
+
+    """
+
+    def __init__(self, username, password, email_address, message):
+        """Initializes an instance of email.
+
+        Inputs:
+            username - gmail address
+            password - password for gmail address
+            email_address - An email address
+                            ex: buggsbunny@gmail.com
+                                porkypig@yahoo.com
+            message - String containing message to send
+
+        Outputs:
+            none
+        """
+        self.username = username
+        self.password = password
+        self.email_address = email_address
+        self.message = message
+        self.server = self.login()
+
+    def login(self):
+        """Logs into gmail account and returns an instance of an SMTP server
+        object.
+        
+        Inputs:
+            none
+
+        Outputs:
+            server - An SMTP server object that can be used to send emails and
+                     messages
+        """
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self.username, self.password)
+        return server
+
+    def email(self, email_address, message):
+        """Method to send an email message (currently text only)
+
+        Inputs:
+            email_address - An email address
+                            ex: buggsbunny@gmail.com
+                                porkypig@yahoo.com
+
+            message - String containing message to send
+        """
+        self.server.sendmail(self.username, self.email_address, self.message)
+
+
+
+
---
+++ messaging/Sms.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import smtplib
+
+"""Sms.py
+
+Class that can send sms messages from a gmail account to a US cell phone.
+
+"""
+
+class Sms(object):
+    """Main class for sending sms messages. 
+
+    """
+
+    def __init__(self, username, password, phone_address, message):
+        """Initializes an instance of Sms.
+
+        Inputs:
+            username - gmail address
+            password - password for gmail address
+            phone_address - String with number and domain of a cell phone
+                           ex: 1234567890@txt.att.net #att
+                               1234567890@tmomail.net #tmobile
+                               1234567890@page.nextel.com #sprint pcs
+                               1234567890@vtext.com #verizon
+        Outputs:
+            none
+        """
+        self.username = username
+        self.password = password
+        self.phone_address = phone_address
+        self.message = message
+        self.server = self.login()
+
+    def login(self):
+        """Logs into gmail account and returns an instance of an SMTP server
+        object.
+        
+        Inputs:
+            none
+
+        Outputs:
+            server - An SMTP server object that can be used to send emails and
+                     messages
+        """
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(self.username, self.password)
+        return server
+
+    def send(self):
+        """Method to send sms messages (text only). 
+
+        Only works for US numbers (assumes country code is 1).
+        """
+        self.server.sendmail(self.username, self.phone_address, self.message)
+
+
